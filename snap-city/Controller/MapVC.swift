464496d6050c9c56b03351ff6c4b099604f2f456
bshakir <burhanshakir96@gmail.com>
2018-04-19 17:16:44 +0100

    Initial commit

---
+++ snap-city/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snap-city
+//
+//  Created by Burhanuddin Shakir on 19/04/18.
+//  Copyright © 2018 COMP47390-41550. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
