7c2886854f23428fdb8267886e52319bd8fe98ee
bshakir <burhanshakir96@gmail.com>
2018-04-21 14:54:09 +0100

    User location on map

--- snap-city/Controller/MapVC.swift
+++ snap-city/Controller/MapVC.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius:Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func locationBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse{
+            centerMapOnUserLocation()
+        }
+    }
 
 }
+
+extension MapVC : MKMapViewDelegate{
+    
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius, regionRadius)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+}
+
+extension MapVC : CLLocationManagerDelegate{
+    func configureLocationServices(){
+        
+        if authorizationStatus == .notDetermined{
+            locationManager.requestWhenInUseAuthorization()
+        }
+        else{
+            return
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
+
+
 
